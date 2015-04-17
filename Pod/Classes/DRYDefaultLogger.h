2aed804f6fc549148dcd52d001cae2a4ac2e4653
Michael Seghers <mike.seghers@appfoundry.be>
2015-04-18 00:47:29 +0200

    More API docs (WIP)

--- Pod/Classes/DRYDefaultLogger.h
+++ Pod/Classes/DRYDefaultLogger.h
@@ -11,13 +11,41 @@
 
 @protocol DRYLoggingAppender;
 
+/**
+ *  Default implementation of the DRYLogger protocol.
+ *
+ *  @see DRYLogger
+ *  @since 1.0
+ */
 @interface DRYDefaultLogger : NSObject<DRYLogger>
 
+/**
+ *  Convenience initializer.
+ *
+ *  @see initWithName:
+ */
 + (instancetype)loggerWithName:(NSString *)name;
+
+/**
+ *  Convenience initializer.
+ *
+ *  @see initWithName:parent:
+ */
 + (instancetype)loggerWithName:(NSString *)name parent:(id<DRYLogger>)parent;
 
+/**
+ *  Creates a logger with the given name, without parent.
+ *
+ *  @param name the name for the logger that should be created.
+ */
 - (instancetype)initWithName:(NSString *)name;
-- (instancetype)initWithName:(NSString *)name parent:(id<DRYLogger>)parent;
 
+/**
+ *  Creates a logger with the given name and parent.
+ *
+ *  @param name   the name for the logger that should be created.
+ *  @param parent the parent for the logger that should be created.
+ */
+- (instancetype)initWithName:(NSString *)name parent:(id<DRYLogger>)parent;
 
 @end
--- Pod/Classes/DRYLoggingAppenderFilter.h
+++ Pod/Classes/DRYLoggingAppenderFilter.h
@@ -10,15 +10,41 @@
 
 @class DRYLoggingMessage;
 
-typedef NS_ENUM(NSInteger, DRYLoggingAppenderFilterDecission) {
+/**
+ *  The result of a filter's decision on what to do with a message.
+ *
+ *  @since 1.1
+ */
+typedef NS_ENUM(NSInteger, DRYLoggingAppenderFilterDecission){
+    /**
+     *  Returned when the filter accepts the message.
+     */
     DRYLoggingAppenderFilterDecissionAccept,
+    /**
+     *  Returned when the filter can not decide what to do with the message.
+     */
     DRYLoggingAppenderFilterDecissionNeutral,
+    /**
+     *  Returned when the filter denies the message.
+     */
     DRYLoggingAppenderFilterDecissionDeny
 };
 
-
+/**
+ *  A filter, used by DRYLoggingAppender instances, to decide whether they should append a message to their underlying 
+ *  system or not.
+ *
+ *  @since 1.1
+ */
 @protocol DRYLoggingAppenderFilter <NSObject>
 
+/**
+ *  When the message is accepted by this filter, returns DRYLoggingAppenderFilterDecissionAccept.
+ *  When the filter cannot decide on what to do with the message, returns DRYLoggingAppenderFilterDecissionNeutral
+ *  When the message is denied by this filter, returns DRYLoggingAppenderFilterDecissionDeny
+ *
+ *  @param message The message to make a decission for.
+ */
 - (DRYLoggingAppenderFilterDecission)decide:(DRYLoggingMessage *)message;
 
 @end
