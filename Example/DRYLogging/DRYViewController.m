d02b9047e78580fa6395675a47b9a820d271ee6d
Michael Seghers <mike.seghers@appfoundry.be>
2015-04-19 01:58:29 +0200

    Corrected the logging macros and applied them on the view controller, also added an easy way to show the mechanism of the rolling file logging

--- Example/DRYLogging/DRYViewController.m
+++ Example/DRYLogging/DRYViewController.m
@@ -21,33 +21,36 @@
 {
     [super viewDidLoad];
     _logger = [DRYLoggerFactory loggerWithName:@"viewcontroller.DRYViewController"];
-    [_logger trace:@"Controller view did load"];
+    DRYTrace(_logger, @"Controller view did load");
 }
 
 - (IBAction)_logATrace:(id)sender {
-    [_logger trace:@"Trace action invoked"];
+    DRYTrace(_logger, @"Trace action invoked");
 }
 
-
 - (IBAction)_logADebug:(id)sender {
-    [_logger debug:@"Debug action invoked"];
+    DRYDebug(_logger, @"Debug action invoked");
 }
 
 - (IBAction)_logAnInfo:(id)sender {
-    [_logger info:@"Info action invoked"];
+    DRYInfo(_logger, @"Info action invoked");
 }
 
 - (IBAction)_logAWarn:(id)sender {
-    [_logger warn:@"Warn action invoked"];
+    DRYWarn(_logger, @"Warn action invoked");
 }
 
 - (IBAction)_logAnError:(id)sender {
-    [_logger error:@"Error action invoked"];
+    NSString *loremIpsum = @"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Curabitur blandit tempus porttitor. Maecenas sed diam eget risus varius blandit sit amet non magna.\n\nEtiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nMaecenas sed diam eget risus varius blandit sit amet non magna. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue.\n\nDonec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus.";
+    
+    for (int i = 0; i < 100; i++) {
+        DRYError(_logger, @"Error action invoked, let's log a lot so we can show off the rolling file mechanism: %@", loremIpsum);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    [_logger warn:@"Memory warning!"];
+    DRYWarn(_logger, @"Memory warning!");
 }
 
 @end
--- Pod/Classes/DRYLogging.h
+++ Pod/Classes/DRYLogging.h
@@ -26,41 +26,41 @@
 #ifndef Pods_DRYLogging_h
 #define Pods_DRYLogging_h
 
-#define _DRYLog(LOGGER, LEVEL, FORMAT, ...) [(LOGGER) LEVEL##WithLineNumber:(__LINE__) format:(FORMAT), __VA_ARGS__]
+#define _DRYLog(LOGGER, LEVEL, ...) [(LOGGER) LEVEL##WithLineNumber:(__LINE__) format:__VA_ARGS__]
 
 /**
  *  Calls the given logger's traceWithLineNumber:format: method, using the current line number of the code on which this call is made and the given format.
  *
  *  @since 1.1
  */
-#define DRYTrace(logger, format, ...) _DRYLog(logger, trace, format, __VA_ARGS__)
+#define DRYTrace(logger, ...) _DRYLog(logger, trace, __VA_ARGS__)
 
 /**
  *  Calls the given logger's debugWithLineNumber:format: method, using the current line number of the code on which this call is made and the given format.
  *
  *  @since 1.1
  */
-#define DRYDebug(logger, format, ...) _DRYLog(logger, debug, format, __VA_ARGS__)
+#define DRYDebug(logger, ...) _DRYLog(logger, debug, __VA_ARGS__)
 
 /**
  *  Calls the given logger's infoWithLineNumber:format: method, using the current line number of the code on which this call is made and the given format.
  *
  *  @since 1.1
  */
-#define DRYInfo(logger, format, ...) _DRYLog(logger, info, format, __VA_ARGS__)
+#define DRYInfo(logger, ...) _DRYLog(logger, info, __VA_ARGS__)
 
 /**
  *  Calls the given logger's warnWithLineNumber:format: method, using the current line number of the code on which this call is made and the given format.
  *
  *  @since 1.1
  */
-#define DRYWarn(logger, format, ...) _DRYLog(logger, warn, format, __VA_ARGS__)
+#define DRYWarn(logger, ...) _DRYLog(logger, warn, __VA_ARGS__)
 
 /**
  *  Calls the given logger's errorWithLineNumber:format: method, using the current line number of the code on which this call is made and the given format.
  *
  *  @since 1.1
  */
-#define DRYError(logger, format, ...) _DRYLog(logger, error, format, __VA_ARGS__)
+#define DRYError(logger, ...) _DRYLog(logger, error, __VA_ARGS__)
 
 #endif
